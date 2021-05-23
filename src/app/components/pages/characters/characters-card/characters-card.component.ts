3050038e6359b48c0f41281182b9e42c7e8a15fa
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 14:56:59 -0700

    UPD Character card

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interfaces/data.interface';
 
 @Component({
   selector: 'app-characters-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./characters-card.component.scss']
 })
 export class CharactersCardComponent implements OnInit {
-
+  @Input() character: Character | undefined;
   constructor() { }
 
   ngOnInit(): void {
