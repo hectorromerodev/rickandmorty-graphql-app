cd09a2fdaaea0a33041895e693bacedcff04e791
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 12:08:34 -0700

    UPD App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ const routes: Routes = [
   { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
   { path: 'not', loadChildren: () => import('./components/notFound/not-found/not-found.module').then(m => m.NotFoundModule) },
   { path: 'character-list', loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
-  { path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) }
+  { path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) },
+  { path: 'about', loadChildren: () => import('./components/pages/about/about/about.module').then(m => m.AboutModule) }
 ];
 
 @NgModule({
