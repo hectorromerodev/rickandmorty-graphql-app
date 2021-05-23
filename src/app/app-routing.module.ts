7a82fbbdd32b126c2c36021b5a902e785305b724
Hector Romero <hectorromerodev@gmail.com>
2021-05-22 23:26:48 -0700

    ADD App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) }
 ];
 
 @NgModule({
