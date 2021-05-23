485ff1f689124741d24b81f7affac794c748658f
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 12:05:28 -0700

    UPD App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
   { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
-  { path: 'not', loadChildren: () => import('./components/notFound/not-found/not-found.module').then(m => m.NotFoundModule) }
+  { path: 'not', loadChildren: () => import('./components/notFound/not-found/not-found.module').then(m => m.NotFoundModule) },
+  { path: 'character-list', loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
+  { path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) }
 ];
 
 @NgModule({
