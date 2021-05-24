f7fc5fd78054a967b06cea686cbe15abe83affee
Hector Romero <hectorromerodev@gmail.com>
2021-05-24 09:38:52 -0700

    UPD Not found component html template literals added

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `
+    <section class="page__notFound">
+      <h1>404</h1>
+      <P>Oh no! You are lost.</P>
+      <img height="400" width="600" src="assets/imgs/404.svg" alt="404">
+    </section>
+  `
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NotFoundComponent { }
