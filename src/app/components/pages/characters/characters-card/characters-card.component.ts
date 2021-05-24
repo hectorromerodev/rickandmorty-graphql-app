a4c31beac1da3a364e15e234a796014da4f97123
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 17:08:19 -0700

    UPD Characters-card

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Character } from '@app/shared/interfaces/data.interface';
 
 @Component({
   selector: 'app-characters-card',
   templateUrl: './characters-card.component.html',
-  styleUrls: ['./characters-card.component.scss']
+  styleUrls: ['./characters-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CharactersCardComponent implements OnInit {
+export class CharactersCardComponent {
   @Input() character: Character | undefined;
-  constructor() { }
+  getIcon(): string {
+    return this.character?.isFavorite ? 'heart-solid.svg' : 'heart.svg';
+  }
 
-  ngOnInit(): void {
+  toggleFavorite(): void {
+    const isFavorite: boolean = this.character?.isFavorite ?? false;
+    this.getIcon();
+    // this.character?.isFavorite = !isFavorite;
   }
 
 }
