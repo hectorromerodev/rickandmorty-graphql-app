3eed8092990b938b4e7961ccd27aa3768ba303ac
Hector Romero <hectorromerodev@gmail.com>
2021-05-24 11:34:32 -0700

    UPD Search component

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { DataService } from '@app/shared/services/data.service';
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  search = new FormControl('');
+  constructor(private dataServ: DataService) {
+    this.search.valueChanges.pipe(
+      map(search => search?.toLowerCase().trim()),
+      debounceTime(300),
+      distinctUntilChanged(),
+      filter(search => search !== '' && search?.length > 2),
+      tap(search => this.dataServ.filterData(search))
+    ).subscribe();
+  }
 
   ngOnInit(): void {
   }
