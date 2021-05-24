86c858457e31c04391732ada8064ae5c49574f78
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 19:05:55 -0700

    UPD Scroll up added with hostlistener

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 import { DataService } from '@app/shared/services/data.service';
 import { LocalStorageService } from '@app/shared/services/local-storage.service';
 
@@ -7,14 +8,29 @@ import { LocalStorageService } from '@app/shared/services/local-storage.service'
   template: `
     <section class="character__list">
       <app-characters-card *ngFor="let character of (characters$ | async)" [character]="character"></app-characters-card>
+      <figure *ngIf="showScrollButton" class="scroll">
+        <img (click)="onScrollTop()" src="assets/imgs/scroll_up.svg" alt="Scroll up" rel="preload" as="image" width="40" height="40">
+      </figure>
     </section>
   `,
   styleUrls: ['./characters-list.component.scss']
 })
 export class CharactersListComponent {
   characters$ = this.dataServ.characters$;
+  showScrollButton = false;
+  private scrollHeight = 500;
   constructor(
     private dataServ: DataService,
-    private localStore: LocalStorageService
+    @Inject(DOCUMENT) private document: Document
   ) { }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    this.showScrollButton = (yOffSet || scrollTop) > this.scrollHeight;
+  }
+  onScrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
 }
