b25ab20e52418128d4a29e5e0ba596742786b86a
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 21:02:31 -0700

    UPD Data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular'
 import { BehaviorSubject } from 'rxjs';
 
-import { tap, take } from 'rxjs/operators'
+import { tap, take, withLatestFrom, pluck } from 'rxjs/operators'
 import { Character, DataResponse, Episode } from '@interfaces/data.interface';
 import { LocalStorageService } from './local-storage.service';
 
@@ -43,6 +43,32 @@ export class DataService {
     this.getDataAPI();
   }
 
+  getCharactersByPage(pageNum: number): void {
+    const QUERY_BY_PAGE = gql`{
+      characters(page: ${pageNum}) {
+        results {
+          id
+          name,
+          status,
+          species,
+          gender,
+          image
+        }
+      }
+    }`;
+
+    this.apollo.watchQuery<any>({
+      query: QUERY_BY_PAGE
+    }).valueChanges.pipe(
+      take(1),
+      pluck('data', 'characters'),
+      withLatestFrom(this.characters$),
+      tap(([apiResponse, characters]) => {
+        this.parseCharactersData([...characters, ...apiResponse.results]);
+      })
+    ).subscribe();
+  }
+
   private getDataAPI(): void {
     // Apollo petition
     this.apollo.watchQuery<DataResponse>({ query: QUERY })
@@ -50,7 +76,7 @@ export class DataService {
         take(1),
         tap(({ data }) => {
           const { characters, episodes } = data;
-          this.charactersSubject.next(characters.results);
+          this.episodesSubject.next(episodes.results);
           this.parseCharactersData(characters.results);
         })
       ).subscribe();
