06c18375e3ba5c81a6b40da5fbc3351d3cff52b9
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 17:47:25 -0700

    UPD Characters card

--- src/app/components/pages/characters/characters-card/characters-card.component.ts
+++ src/app/components/pages/characters/characters-card/characters-card.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Character } from '@app/shared/interfaces/data.interface';
+import { LocalStorageService } from '@app/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-characters-card',
@@ -8,15 +9,19 @@ import { Character } from '@app/shared/interfaces/data.interface';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CharactersCardComponent {
-  @Input() character: Character | undefined;
+  @Input() character!: Character;
+  constructor(
+    private localStgServ: LocalStorageService
+  ) { }
   getIcon(): string {
     return this.character?.isFavorite ? 'heart-solid.svg' : 'heart.svg';
   }
 
   toggleFavorite(): void {
-    const isFavorite: boolean = this.character?.isFavorite ?? false;
+    const isFavorite = !!this.character?.isFavorite;
     this.getIcon();
-    // this.character?.isFavorite = !isFavorite;
+    this.character.isFavorite = !isFavorite;
+    this.localStgServ.addOrRemoveFavorite(this.character);
   }
 
 }
