7b0a1b6920c570df4caad37555285a9efce0484f
Hector Romero <hectorromerodev@gmail.com>
2021-05-24 16:05:19 -0700

    UPD Header label menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styles: [`
+    .hide{
+      display: none;
+    }
+  `]
 })
 export class HeaderComponent { }
