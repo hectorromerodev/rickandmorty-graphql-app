82e7010e89c0920deb979c0754e4eaec396d4db8
Hector Romero <hectorromerodev@gmail.com>
2021-05-24 09:39:16 -0700

    UPD Home component template literals

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { LocalStorageService } from '@app/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `
+    <section class="character__list">
+      <app-characters-card *ngFor="let character of charactersFav$ | async" [character]="character"></app-characters-card>
+    </section>
+  `,
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   charactersFav$ = this.localStgServ.charactersFav$;
   constructor(private localStgServ: LocalStorageService) { }
-
-  ngOnInit(): void {
-  }
-
 }
