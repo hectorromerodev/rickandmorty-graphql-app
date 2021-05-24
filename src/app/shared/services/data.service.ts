a994d9fde816feb2510ac4e1f14b420149e8b48e
Hector Romero <hectorromerodev@gmail.com>
2021-05-23 17:40:17 -0700

    UPD Data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject } from 'rxjs';
 
 import { tap, take } from 'rxjs/operators'
 import { Character, DataResponse, Episode } from '@interfaces/data.interface';
+import { LocalStorageService } from './local-storage.service';
 
 const QUERY = gql`
   {
@@ -36,7 +37,8 @@ export class DataService {
   characters$ = this.charactersSubject.asObservable();
 
   constructor(
-    private apollo: Apollo
+    private apollo: Apollo,
+    private localStorage: LocalStorageService
   ) {
     this.getDataAPI();
   }
@@ -48,9 +50,18 @@ export class DataService {
         take(1),
         tap(({ data }) => {
           const { characters, episodes } = data;
-          this.episodesSubject.next(episodes.results);
           this.charactersSubject.next(characters.results);
+          this.parseCharactersData(characters.results);
         })
       ).subscribe();
+  }
+
+  private parseCharactersData(characters: Character[]): void {
+    const currentFavs = this.localStorage.getFavoritesCharacters();
+    const newData = characters.map(character => {
+      const found = !!currentFavs.find((fav: Character) => fav.id === character.id);
+      return { ...character, isFavorite: found };
+    });
+    this.charactersSubject.next(newData);
   }
 }
